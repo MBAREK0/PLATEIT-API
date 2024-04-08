04261585c3e6b83e68b2b750c1ed2087244e023a
Mbarek Elaadraoui <elaadraouimbarek2023@gmail.com>
2024-04-08 16:52:49 +0000

    finiched the left sidebar

--- plateit-front/src/components/MainLayout.vue
+++ plateit-front/src/components/MainLayout.vue
@@ -1,10 +1,8 @@
 <!-- MainLayout.vue -->
 <template>
   <body class=" inter bg-main_color dark:bg-main_color_dark w-full h-screen m-0 p-0 overflow-x-hidden text-main_text_color dark:text-sidebar_text_color ">
-      <div class="left-0 bg-sidebar_color dark:bg-sidebar_color_dark lg:block md:block hidden  fixed top-0 bottom-0  w-1/5 text-sidebar_text_color">
-        <div class="flex flex-col items-center">
-          <img src="../assets/logo.png" alt="logo" class="mt-10 w-10/12">
-        </div> 
+      <div class="left-0 bg-sidebar_color dark:bg-sidebar_color_dark lg:block md:block hidden  fixed top-0 bottom-0  w-1/5 text-sidebar_text_color h-screen ">
+ 
         <LeftSidebar/>
 
       </div>
